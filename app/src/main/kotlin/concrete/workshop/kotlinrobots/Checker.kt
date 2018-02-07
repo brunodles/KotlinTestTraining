21a1354344e98d58ef9a083fa2c4a315ddd8b50f
Bruno de Lima e Silva <bruno.silva@concrete.com.br>
2018-02-07 17:36:25 -0200

    Initial commit

---
+++ app/src/main/kotlin/concrete/workshop/kotlinrobots/Checker.kt
@@ -0,0 +1,10 @@
+package concrete.workshop.kotlinrobots
+
+interface Checker {
+
+    fun sun(first: Int, second: Int): Int
+
+    fun isInteger(value: String): Boolean
+
+
+}
---
+++ app/src/main/kotlin/concrete/workshop/kotlinrobots/CheckerProvider.kt
@@ -0,0 +1,8 @@
+package concrete.workshop.kotlinrobots
+
+object CheckerProvider {
+
+    fun getChecker(): Checker {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
